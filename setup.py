3c17f9d6fbefb01a2eee654a75b3253103e17474
marmarmarmar <marmarmarmar@users.noreply.github.com>
2022-03-09 16:05:32 +0100

    Added setup.py (#10)
    Co-authored-by: Marcin Możejko <marcinmozejko@MacBook-Air-Marcin.local>

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import find_packages
+from setuptools import setup
+
+setup(
+    name='LSO',
+    version='0.1',
+    description='Python package for a latent space optimization.',
+    author='Marcin Możejko & Adam Izdebski',
+    author_email='mmozejko1988@gmail.com',
+    url='https://github.com/szczurek-lab/lso',
+    packages=find_packages(),
+    install_requires=[]
+)
