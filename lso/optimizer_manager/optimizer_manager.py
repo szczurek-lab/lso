5f37df0942150b249a96edc934e7f0558964aa21
Marcin Możejko <marcinmozejko@MacBook-Air-Marcin.local>
2022-03-09 18:04:54 +0100

    Added OptimizerManager interface.

---
+++ lso/optimizer_manager/optimizer_manager.py
@@ -0,0 +1,17 @@
+from abc import ABC
+
+from lso.data import data as lso_data
+from lso.data_manager import data_manager as lso_data_manager
+from lso.model_manager import model_manager as lso_model_manager
+
+
+class OptimizerManager(ABC):
+
+    def get_candidates(
+        self,
+        model_manager: lso_model_manager.ModelManager,
+        data_manager: lso_data_manager.DataManager,
+        epoch_nb: int,
+        nb_of_candidates: int
+    ) -> lso_data.Data:
+        raise NotImplementedError
