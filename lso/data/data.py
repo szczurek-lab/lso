228750ab5da56922ced4bb53bf08bb4ef9b8247f
Marcin Możejko <marcinmozejko@MacBook-Air-Marcin.local>
2022-03-09 12:30:25 +0100

    Added a Data class.

---
+++ lso/data/data.py
@@ -0,0 +1,24 @@
+from abc import ABC
+from dataclasses import dataclass
+from typing import Any
+from typing import Optional
+from typing import Tuple
+
+
+@dataclass
+class Data(ABC):
+    x: Any
+    objective: Optional[Any] = None
+    features: Optional[Any] = None
+
+    def __len__(self) -> int:
+        return len(self.x)
+
+    def __getitem__(self, idx: int) -> Tuple:
+        x_idx = self.x[idx]
+        objective_idx = self.objective[idx] if self.objective is not None else None
+        features_idx = self.features[idx] if self.features is not None else None
+        return x_idx, objective_idx, features_idx
+
+    def __add__(self, other: Any) -> "Data":
+        raise NotImplementedError
