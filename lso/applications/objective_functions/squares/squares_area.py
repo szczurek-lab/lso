fd035ae746fe6da53f83372f9f3310d5a71249e3
AdamIzdebski <adam.izdebski1@gmail.com>
2022-03-15 09:19:09 +0100

    implement comments.

--- lso/applications/objective_functions/squares/squares_area.py
+++ lso/applications/objective_functions/squares/squares_area.py
@@ -1,46 +1,36 @@
 import numpy as np
 from typing import Optional
 
-from lso.utils import area_utils
+from lso.applications.utils.squares import utils
 from lso.data import data as lso_data
 from lso.objective_function import objective_function as lso_objective_function
 
 
-class SquaresArea(lso_objective_function.ObjectiveFunction):
+class PenalizedJaccardShapeArea(lso_objective_function.ObjectiveFunction):
 
     def __init__(self, shape_likeliness_coeff: Optional[float] = 1.0):
         self.shape_likeliness_coeff = shape_likeliness_coeff
 
-    def calculate_shape_likeliness(self,
-                                   x: np.array,
-                                   metric: Optional[str] = 'Jaccard') -> np.array:
+    def calculate_jaccard_penalty(self, x: np.array) -> np.array:
 
-        if metric == 'Jaccard':
+        # Compute the Jaccard distance for areas of: shape x and the square circumscribed on shape x.
 
-            # Compute the Jaccard distance for areas of: shape x ,and the square circumscribed on shape x.
+        area_x = utils.calculate_area(x)
+        area_circumscribed_x = utils.calculate_circumscribed_square_area(x)
 
-            area_x = area_utils.calculate_area(x)
-            area_circumscribed_x = area_utils.calculate_circumscribed_square_area(x)
-
-            jaccard_index = area_x / area_circumscribed_x
-            shape_likeliness = 1 - jaccard_index
-
-        if metric not in ['Jaccard']:
-            raise ValueError('provide a correct metric.')
+        jaccard_index = area_x / area_circumscribed_x
+        shape_likeliness = 1 - jaccard_index
 
         return shape_likeliness
 
     def evaluate(self, data: lso_data.Data) -> lso_data.Data:
 
-        assert issubclass(type(data), lso_data.Data)
         assert len(data.x.shape) == 3
 
-        area = area_utils.calculate_area(data.x)
-        shape_likeliness = self.shape_likeliness_coeff * self.calculate_shape_likeliness(data.x)
-        objective = area - shape_likeliness
+        area = utils.calculate_area(data.x)
+        penalty = self.shape_likeliness_coeff * self.calculate_jaccard_penalty(data.x)
+        objective = area - penalty
 
-        return type(data)(
-            x=data.x,
-            objective=objective,
-            features=data.features
-        )
+        data.objective = objective
+
+        return data
