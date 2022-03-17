6684d3db9ad143d505c6424037b7a9f716847e26
marmarmarmar <marmarmarmar@users.noreply.github.com>
2022-03-17 17:35:53 +0100

    Added vectorizer usage to a model_manager. (#54)
    Co-authored-by: Marcin Możejko <marcinmozejko@MacBook-Air-Marcin.local>

--- lso/model_manager/model_manager.py
+++ lso/model_manager/model_manager.py
@@ -4,11 +4,15 @@ from typing import List
 from typing import Optional
 
 from lso.data import data as lso_data
+from lso.data import vectorizer as lso_vectorizer
 from lso.data_manager import data_manager as lso_dm
 from lso.model import model as lso_model
 
 
 class ModelManager(ABC):
+
+    def __init__(self, vectorizer: lso_vectorizer.Vectorizer):
+        self.vectorizer = vectorizer
 
     def train(self, data_manager: lso_dm.DataManager, epoch_nb: int):
         raise NotImplementedError
@@ -28,8 +32,10 @@ class SingleModelModelManager(ModelManager, ABC):
     def __init__(
             self,
             model: lso_model.Model,
+            vectorizer: lso_vectorizer.Vectorizer,
             instance_params: Optional[List] = None,
     ):
+        super().__init__(vectorizer=vectorizer)
         self.model = model
         self.instance_params = instance_params if instance_params is not None else []
 
@@ -38,8 +44,10 @@ class SingleModelModelManager(ModelManager, ABC):
 
     def encode(self, data: lso_data.Data, epoch_nb: int) -> lso_data.Latent:
         model_instance = self.model.get_instance(self.instance_params[epoch_nb])
-        return model_instance.encode(data=data)
+        vectorized_data = self.vectorizer.encode(data)
+        return model_instance.encode(data=vectorized_data)
 
     def decode(self, latent: lso_data.Latent, epoch_nb) -> lso_data.Data:
         model_instance = self.model.get_instance(self.instance_params[epoch_nb])
-        return model_instance.decode(latent=latent)
+        vectorized_data = model_instance.decode(latent=latent)
+        return self.vectorizer.decode(vectorized_data)
