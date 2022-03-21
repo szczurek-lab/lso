b481d5c4ca3f4693e7920d4f5a4a0f2d1a432300
Marcin Możejko <marcinmozejko@MacBook-Air-Marcin.local>
2022-03-21 16:51:29 +0100

    Added model serialization.

--- lso/applications/models/model_serialization.py
+++ lso/applications/models/model_serialization.py
@@ -1,16 +1,22 @@
 from functools import singledispatch
 
+from lso.applications.models.torch import pca_models
 from lso.model import model as lso_model
 from lso.utils import io as lso_io_utils
 
 # Insert new model types here:
-MODEL_TYPES = []
+MODEL_TYPES = [
+    pca_models.SimplePCATorchModel,
+]
 MODEL_NAME_TO_TYPE = {type_.__name__: type_ for type_ in MODEL_TYPES}
 
 
 @singledispatch
 def save_model(model: lso_model.Model, path: str):
-    raise NotImplementedError(f'Model serialization not implemented for {type(model).__name__}.')
+    Warning(f'Default model serialization run for {type(model).__name__}.')
+    lso_io_utils.create_path(path=path)
+    lso_io_utils.save_type_name_to_path(obj=model, path=path)
+    lso_io_utils.save_config_dict_to_path(config_dict=model.get_config_dict(), path=path)
 
 
 def load_model(path: str):
